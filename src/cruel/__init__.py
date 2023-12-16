bd62de680ee38aa7c2145936751db53b90e0eb76
ccdale <chris.allison@hivehome.com>
2023-12-16 10:49:38 +0000

    Bumping version from 0.1.4 to 0.1.5

--- src/cruel/__init__.py
+++ src/cruel/__init__.py
@@ -20,7 +20,7 @@ import sys
 
 import ccalogging
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __appname__ = "cruel"
 
 ccalogging.setConsoleOut()
--- tests/test_cruel.py
+++ tests/test_cruel.py
@@ -34,7 +34,7 @@ class TheException(Exception):
 
 
 def test_cruel_version():
-    assert __version__ == "0.1.4"
+    assert __version__ == "0.1.5"
 
 
 def test_errorNotify(caplog):
