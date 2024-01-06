322705570fa706333646e75ea76b43ad1028b0e8
ccdale <chris.allison@hivehome.com>
2024-01-06 13:30:16 +0000

    adjust tests for classes as seperate modules

--- tests/test_cardname.py
+++ tests/test_cardname.py
@@ -16,7 +16,6 @@
 #     You should have received a copy of the GNU General Public License
 #     along with cruel.  If not, see <http://www.gnu.org/licenses/>.
 #
-from cruel import playingcards as pc
 from cruel.cardname import CardName
 
 
